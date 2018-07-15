f1594b1dc5af759939c226e590ab359f06b91399
TheAllenAnker <theallenanker@gmail.com>
2018-07-15 16:23:04 +0800

    Formalize the data acquired from querying API.

---
+++ app/view_models/book.py
@@ -0,0 +1,41 @@
+# Author: Allen Anker
+# Created by Allen Anker on 15/07/2018
+
+
+class BookViewModel:
+    @classmethod
+    def package_single(cls, data, keyword):
+        returned = {
+            'books': [],
+            'total': 0,
+            'keyword': keyword
+        }
+        if data:
+            returned['total'] = 1
+            returned['books'] = [cls.__cut_book_data(data)]
+        return returned
+
+    @classmethod
+    def package_collection(cls, data, keyword):
+        returned = {
+            'books': [],
+            'total': 0,
+            'keyword': keyword
+        }
+        if data:
+            returned['total'] = data['total']
+            returned['books'] = [cls.__cut_book_data(book) for book in data['books']]
+        return returned
+
+    @classmethod
+    def __cut_book_data(cls, data):
+        book = {
+            'title': data['title'],
+            'publisher': data['publisher'],
+            'pages': data['pages'] or '',
+            'author': ' '.join(data['author']),
+            'price': data['price'],
+            'summary': data['summary'] or '',
+            'image': data['image']
+        }
+        return book
--- app/web/book.py
+++ app/web/book.py
@@ -3,6 +3,8 @@
 
 
 from flask import jsonify, request
+
+from app.view_models.book import BookViewModel
 from . import web
 from app.libs.helper import is_isbn_or_key
 from app.spider.yushu_book import YuShuBook
@@ -23,8 +25,10 @@ def search():
         isbn_or_key = is_isbn_or_key(q)
         if isbn_or_key == 'isbn':
             result = YuShuBook.search_by_isbn(q)
+            result = BookViewModel.package_single(result, q)
         else:
             result = YuShuBook.search_by_keyword(q, page)
+            result = BookViewModel.package_collection(result, q)
         return jsonify(result)
     else:
         return jsonify(form.errors)
