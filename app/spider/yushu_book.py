6f10692ee2e5d1ab8e7ddb02d00adf82f64dcec3
TheAllenAnker <theallenanker@gmail.com>
2018-07-15 17:10:51 +0800

    Refactoring.

--- app/spider/yushu_book.py
+++ app/spider/yushu_book.py
@@ -6,25 +6,38 @@ from flask import current_app
 
 class YuShuBook:
     """
-    Acquire data from API
+    Acquire and store books data from API
     """
     # API addresses
     isbn_url = 'http://t.yushu.im/v2/book/isbn/{}'
     keyword_url = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
 
-    @classmethod
-    def search_by_isbn(cls, isbn):
-        url = cls.isbn_url.format(isbn)
+    def __init__(self):
+        self.total = 0
+        self.books = []
+
+    def search_by_isbn(self, isbn):
+        url = self.isbn_url.format(isbn)
         result = HTTP.get(url)
+        self.__fill_single(result)
         return result
 
-    @classmethod
-    def search_by_keyword(cls, keyword, page=1):
-        url = cls.keyword_url.format(keyword, current_app.config['PER_PAGE'],
-                                     cls.calculate_start(page))
+    def search_by_keyword(self, keyword, page=1):
+        url = self.keyword_url.format(keyword, current_app.config['PER_PAGE'],
+                                      self.__calculate_start(page))
         result = HTTP.get(url)
+        self.__fill_collection(result)
         return result
 
     @staticmethod
-    def calculate_start(page):
+    def __calculate_start(page):
         return (page - 1) * current_app.config['PER_PAGE']
+
+    def __fill_single(self, data):
+        if data:
+            self.total = 1
+            self.books.append(data)
+
+    def __fill_collection(self, data):
+        self.total = data['total']
+        self.books = data['books']
